4ec37e90d65eb3584b6c00ac8814b14307163ae4
Stu <stuart@blueprintinteractive.com>
2015-11-20 23:16:47 -0500

    basic functionality completed

--- src/site.js
+++ src/site.js
@@ -4,9 +4,31 @@
     angular.module('ngpApp', [])
         .controller('formController', formController);
 
-    formController.$inject = [];
-    function formController() {
-        
+    formController.$inject = ['$http'];
+    function formController($http) {
+        var vm = this;
+
+        function getFormId(url) {
+
+
+        }
+
+        vm.submitForm = function(url) {
+            var formId = url.match(/-?[0-9]{10,}/g);
+
+            $http({
+                url: 'http://localhost:3000/api',
+                method: 'GET',
+                params: {
+                    formId: formId
+                }
+            }).then(function(response) {
+                console.log(response.data);
+                vm.response = response.data;
+            });
+        }
+
+
     }
 
 }());
