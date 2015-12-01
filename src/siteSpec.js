3c568af20a458ce4a123badef568836087c87eca
Stu <stuart@blueprintinteractive.com>
2015-12-01 09:20:11 -0500

    more progress on test

--- src/siteSpec.js
+++ src/siteSpec.js
@@ -1,20 +1,33 @@
 describe("ngpApp", function() {
   beforeEach(module("ngpApp"));
 
-  beforeEach(inject(function($controller, $rootScope) {
+  beforeEach(inject(function($controller, $rootScope, $q, $httpBackend) {
     scope = $rootScope.$new();
     ctrl = $controller('formController', { $scope: scope });
+    deferred = $q.defer();
+    $httpBackend = $httpBackend;
   }));
 
   describe("formController", function() {
-    it('should fetch form details', function() {
-        scope.name = "Frederik";
-
-        console.log(ctrl);
-        console.log(ctrl.activeTab);
+    it('should set initial tab as html', function() {
 
         scope.$digest();
         expect(ctrl.activeTab).toBe("html");
+
+
     });
+
+    it('should query the backend when the username is checked',
+    inject(function($rootScope, $httpBackend) {
+        $httpBackend.expect('GET', 'http://localhost:3000/api?formId=7944614294361016320').respond({});
+        var status = false;
+
+        ctrl.submitForm("7944614294361016320");
+
+        $rootScope.$digest();
+        $httpBackend.flush();
+        $httpBackend.verifyNoOutstandingRequest();
+    }));
+
   });
 });
