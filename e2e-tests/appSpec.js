d41f9d8d156cb10f775da033c7f597935e0e371d
Stu <stuart@blueprintinteractive.com>
2015-12-20 13:42:04 -0500

    2nd successful e2e test added

--- e2e-tests/appSpec.js
+++ e2e-tests/appSpec.js
@@ -1,11 +1,16 @@
-/*beforeEach(function() {
-  browser.get('http://127.0.0.1:3000/');
-});*/
+beforeEach(function() {
+  browser.get('http://127.0.0.1:8081/');
+});
 
-describe('Protractor Demo App', function() {
+describe('NGP Form Scraper App', function() {
+  
   it('should have a title', function() {
-    browser.get('http://127.0.0.1:8081/');
-
     expect(browser.getTitle()).toEqual('NGP Form Scraper');
   });
+
+  it('should bring up ajax loader on submit', function() {
+    element(by.id('basic-url')).sendKeys('7944614294361016320\n');
+    expect(element(by.id('results')).isDisplayed()).toBe(true);
+  });
+
 });
