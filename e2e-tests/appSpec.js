2d3f672c5340622c2013d2c8b0ae2685e4c2533c
Stu <stuart@blueprintinteractive.com>
2015-12-20 13:51:16 -0500

    add condition to make sure form is hidden after submit to e2e test

--- e2e-tests/appSpec.js
+++ e2e-tests/appSpec.js
@@ -8,9 +8,10 @@ describe('NGP Form Scraper App', function() {
     expect(browser.getTitle()).toEqual('NGP Form Scraper');
   });
 
-  it('should bring up ajax loader on submit', function() {
+  it('should show results and hide form on submit', function() {
     element(by.id('basic-url')).sendKeys('7944614294361016320\n');
     expect(element(by.id('results')).isDisplayed()).toBe(true);
+    expect(element(by.css('form')).isDisplayed()).toBe(false);
   });
 
 });
