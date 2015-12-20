368ba95d7b3284f3820621fd43377fcbbb0f8aa0
Stu <stuart@blueprintinteractive.com>
2015-12-20 14:43:40 -0500

    last e2e test added. hurray

--- e2e-tests/appSpec.js
+++ e2e-tests/appSpec.js
@@ -9,14 +9,12 @@ describe('NGP Form Scraper App', function() {
   });
 
   it('should show results div and hide form on submit', function() {
-    element(by.id('basic-url')).sendKeys("7944614294361016320");
-    element(by.css('input[type=submit]')).click();
-
+    element(by.id('basic-url')).sendKeys("7944614294361016320\n");
     expect(element(by.id('results')).isDisplayed()).toBe(true);
     expect(element(by.css('form')).isDisplayed()).toBe(false);
   });
 
-  it('should show results if formID exists and error message if it does not', function() {
+  it('should show results if formID exists', function() {
     element(by.id('basic-url')).sendKeys('7944614294361016320\n');
     expect(element(by.css('.alert-success')).isDisplayed()).toBe(true);
     expect(element(by.css('.alert-danger')).isDisplayed()).toBe(false);
@@ -31,7 +29,16 @@ describe('NGP Form Scraper App', function() {
     element.all(by.css('button')).get(0).click(); // click html button
     expect(element(by.css('table')).isDisplayed()).toBe(false);
     expect(element(by.css('.results-html')).isDisplayed()).toBe(true);
+  });
 
+  it('should show error message if formID does not exist', function() {
+    element(by.id('basic-url')).sendKeys('123412341234\n');
+    expect(element(by.css('.alert-success')).isDisplayed()).toBe(false);
+    expect(element(by.css('.alert-danger')).isDisplayed()).toBe(true);
+
+    element(by.css('.btn-danger')).click();
+    expect(element(by.id('results')).isDisplayed()).toBe(false);
+    expect(element(by.css('form')).isDisplayed()).toBe(true);
   });
 
 });
